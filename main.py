d5bebe05e43b9905b9c71b3676afcb36f05c9579
Adriwin <adri1.rabot@gmail.com>
2023-09-28 12:57:05 +0200

    Conseils Thomas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import*
+import random
+
 class MonsterMunch():
     def __init__(self, pV=200, pA=30, pD=0, pVitesse=100, Attack={'Charge': 1.2, 'Foudre': 1.7, 'Feu': 1.1}):
         self.pV_orig = pV                       # ← PV originaux (à ne pas changer)
@@ -10,11 +11,12 @@ class MonsterMunch():
         self.degats = 0
         self.Attack = Attack
 
+    # Thomas : perso je conseille de rajouter une fonction __eq__, __str__ et éventuellement __repr__
 
 
 # Définition de la méthode "heal" qui soigne le joueur en fonction d'une quantité
     def heal(self, amount):
-        if self.pV + amount > self.pV_orig:
+        if (self.pV + amount) > self.pV_orig:
             self.pV = self.pV_orig
         self.pV += amount
 
@@ -25,28 +27,29 @@ class MonsterMunch():
         assert isinstance(other, MonsterMunch), "other is not a MonsterMunch"
 
         # Si l'ennemi est plus rapide que toi, augmenter les chances d'esquiver par palier
-        if self.pVitesse - other.pVitesse < 0 < -10:
+        if (self.pVitesse - other.pVitesse) < 0 < -10:
+            # return True if random.random.randint(0, 100) < 15 else False
             if random.randint(0,100) < 15:
                 return True
             return False
         
-        elif self.pVitesse - other.pVitesse < 0 < -20:
+        elif (self.pVitesse - other.pVitesse) < 0 < -20:
             if random.randint(0,100) < 30:
                 return True
             return False
         
-        elif self.pVitesse - other.pVitesse < 0 < -30:
+        elif (self.pVitesse - other.pVitesse) < 0 < -30:
             if random.randint(0,100) < 40:
                 return True
             return False
         
-        elif self.pVitesse - other.pVitesse < -50:
+        elif (self.pVitesse - other.pVitesse) < -50:
             if random.randint(0,100) < 50:
                 return True
             return False
 
-        # Si l'ennemi est plus rapide que toi, augmenter les chances d'esquiver pas palier"      
-        elif self.pVitesse - other.pVitesse > 10:
+        # Si l'ennemi est moins rapide que toi, diminuer les chances d'esquiver pas palier
+        elif (self.pVitesse - other.pVitesse) > 10:
             if random.randint(0,100) < 7:
                 return True
             return False
@@ -64,14 +67,16 @@ class MonsterMunch():
         # Si la défense de l'ennemi est supérieure à l'attaque
         if other.pD > self.pA:
             self.degats_Infl *= random.uniform(0.05, 0.2)
+            # self.degats_Infl *= round(random.uniform(0.05, 0.2), 2) # Il vaut mieux arrondir (avec round) les floats générés aléatoirement parce que sinon la ram peut se faire pomper à mort # round(float, 2) = arrondir le float avec deux chiffres après la virgule
         # Chance de coup critique
-        elif randint(0, 100) == 69:
+        elif random.randint(0, 100) == 69:
             self.degats_Infl *= 1.50
         # Si l'attaque est supérieure à la défense de l'ennemi
         else:
             self.degats_Infl *= random.uniform(0.75, 1.0)
 
-        if MonsterMunch.esquive(self, other) is True:
+        if MonsterMunch.esquive(self, other) is True: # En vrai le "is True" est pas nécessaire
+        # Correction : if self.esquive(other) is True:
             return "L'ennemi vous a pris de vitesse et a esquivé"
         else:
             other.pV -= self.degats_Infl
