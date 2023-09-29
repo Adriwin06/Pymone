80563636b20840fc11084b4814074ee4fe6261f7
Adriwin <adri1.rabot@gmail.com>
2023-09-29 12:44:20 +0200

    amélioration de la doc (pas fini)

--- MonsterFunction.py
+++ MonsterFunction.py
@@ -1,17 +1,17 @@
 import random
 
 class MonsterMunch():
-    def __init__(self, pV=200, pA=30, pD=0, pVitesse=100, Attack={'Charge': 1.2, 'Foudre': 1.7, 'Feu': 1.1}, element='Feu', faiblesse='eau'):
+    def __init__(self, pV=200, pA=30, pD=0, pVitesse=100, Attack={'Charge': 1.2, 'Coup de queue': 1.7, 'Basique': 1.1}, element='Feu', faiblesse='eau'):
         self.pV_orig = pV                       # PV originaux (à ne pas changer)
         self.pV = pV                            # PV actuels
         self.pA = pA                            # puissance d'attaque
         self.pD = pD                            # caractéristique de défense
         self.pVitesse = pVitesse                # vitesse du Monstre
-        self.degats_Infl = 0                    # variable qui stockerant le nombre de dégats infligés par le Monstre
-        self.degats = 0                         # variable qui stockerant le nombre de dégats subi par le Monstre
-        self.Attack = Attack
-        self.element = element
-        self.faiblesse = faiblesse
+        self.degats_Infl = 0                    # variable qui stock le nombre de dégats infligés par le Monstre
+        self.degats = 0                         # variable qui stock le nombre de dégats subi par le Monstre
+        self.Attack = Attack                    # variable qui stock les différentes attaques
+        self.element = element                  # variable qui stock l'élément du monstre
+        self.faiblesse = faiblesse              # variable qui stock la faiblesse du monstre ainsi que sa résistance
 
     # Thomas : perso je conseille de rajouter une fonction __eq__, __str__ et éventuellement __repr__
 
@@ -29,8 +29,7 @@ class MonsterMunch():
         assert isinstance(other, MonsterMunch), "other is not a MonsterMunch"
 
         # Si l'ennemi est plus rapide que toi, augmenter les chances d'esquiver par palier
-        if (self.pVitesse - other.pVitesse) < 0 < -10:
-            # return True if random.random.randint(0, 100) < 15 else False
+        if (self.pVitesse - other.pVitesse) < 0 < -10:                      #on aurait pu faire plus court avec : return True if random.random.randint(0, 100) < 15 else False
             if random.randint(0,100) < 15:
                 return True
             return False
@@ -63,7 +62,7 @@ class MonsterMunch():
 
 
 
-# Définition de la méthode "faiblesse" qui renvoie >>> 'Faible' <<< si l'ennemi est faible, >>> Résistant <<< si l'ennemi est resistant, >>> 'none' si rien ne se passe
+# Définition de la méthode "faiblesse" qui renvoie >>> 'Faible' <<< si l'ennemi est faible, >>> 'Résistant' <<< si l'ennemi est resistant, >>> 'none' si rien ne se passe
     def faiblesse(self, other):
         assert isinstance(other, MonsterMunch), "other is not a MonsterMunch"
         elements_faiblesse = {'Feu': 'Eau', 'Pierre': 'Nature', 'Nature':'Feu', 'Eau':'Pierre'}
@@ -79,36 +78,39 @@ class MonsterMunch():
 
 
 
-# Définition de la méthode "attack" qui
+# Définition de la méthode "attack" qui attaque l'ennemi. Prends en compte les résistance et faiblesse de l'ennmei ainsi que sa vitesse
     def attack(self, other):
         assert isinstance(other, MonsterMunch), "other is not a MonsterMunch"
+        
         # Si la défense de l'ennemi est supérieure à l'attaque
         if other.pD > self.pA:
-            self.degats_Infl *= random.uniform(0.05, 0.2)
-            # self.degats_Infl *= round(random.uniform(0.05, 0.2), 2) # Il vaut mieux arrondir (avec round) les floats générés aléatoirement parce que sinon la ram peut se faire pomper à mort # round(float, 2) = arrondir le float avec deux chiffres après la virgule
+            self.degats_Infl *= round(random.uniform(0.05, 0.2), 2)
+        
         # Chance de coup critique
         elif random.randint(0, 100) == 69:
             self.degats_Infl *= 1.50
+        
         # Si l'attaque est supérieure à la défense de l'ennemi
         else:
             self.degats_Infl *= random.uniform(0.75, 1.0)
 
 
 
-        if MonsterMunch.esquive(self, other) is True: # En vrai le "is True" est pas nécessaire
-        # Correction : if self.esquive(other) is True:
+        if self.esquive(other) is True:                                 # "is True" pas nécessaire mais plus simple à lire
             return "L'ennemi vous a pris de vitesse et a esquivé"
         else:
-            if MonsterMunch.faiblesse(self, other) == 'Faible':
+            if self.faiblesse(other) == 'Faible':
                 self.degats_Infl *= 2
                 other.pV -= self.degats_Infl
                 print(f"Vous avez Infligés {self.degats_Infl} de dégâts à l'ennemis")
                 print("L'ennemi est faible")
-            elif MonsterMunch.faiblesse(self, other) == 'Résistant':
+            
+            elif self.faiblesse(other) == 'Résistant':
                 self.degats_Infl *= 0.5
                 other.pV -= self.degats_Infl
                 print(f"Vous avez Infligés {self.degats_Infl} de dégâts à l'ennemis")
                 print("L'ennemi est résistant")
+            
             else:
                 other.pV -= self.degats_Infl
                 print(f"Vous avez Infligés {self.degats_Infl} de dégâts à l'ennemis")
