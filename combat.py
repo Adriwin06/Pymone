2c542de6f905530b72cec9cfc3f778812fb660e3
PandEwok <diegojaillot@outlook.fr>
2023-10-05 11:04:40 +0200

    fin de méthode debut  (class combat)

--- combat.py
+++ combat.py
@@ -11,20 +11,52 @@ class combat():
         self.inventory1 = self.j1.inventory
         if isinstance(opponent, player):
             self.inventory2 = self.j2.inventory
+            self.Monstre2 = 'rien zebi'
+        else:
+            self.Monstre2 = opponent
         self.Monstre1 = 'rien zebi'
-        
+    
+    
+    # Méthode pour initier le combat ; déclare le début du combat et demande aux joueurs de choisir le monstre qu'ils veulent envoyer au combat
+    # puis l'assigne à Monster1 pour le joueur 1 ou Monster2 pour le joueur 2
     def debut(self):
-        print(f'Un combat commence ! {(self.j1).name} affronte {self.j2.name} ! C\'est l\'heure de choisir vos monstres...')
-        print(f'{(self.j1).name}, c\'est à vous de choisir un combatant. Voici vos monstres :')
+        print(f"Un combat commence ! {(self.j1).name} affronte {self.j2.name} ! C'est l'heure de choisir vos monstres...")
+        
+        # début du combat pour joueur 1 (choix du monstre)
+        print(f"{(self.j1).name}, c'est à vous de choisir un combatant. Voici vos monstres :")
         for i in range(len(self.inventory1)):
             print(f'• {self.inventory1[i].name}')
         while self.Monstre1 not in self.inventory1:
-            self.Monstre1 = input('Lequel voulez-vous envoyer en premier ? ')
+            self.Monstre1 = input("Lequel voulez-vous envoyer en premier ? ")
+        for monstre in self.inventory1:
+            if monstre == self.Monstre1:
+                self.Monstre1 = monstre
+        print(f'Vous avez choisi {self.Monstre1.name} ! {self.Monstre1.name}, go ! \n')
         
+        # début du combat pour le joueur 2 (choix du monstre)
+        # seulement si l'adversaire est un joueur (ou un pnj considéré comme un joueur)
+        if isinstance(self.j2, player):
+            print(f"{(self.j2).name}, c'est à vous de choisir un combatant. Voici vos monstres :")
+            for i in range(len(self.inventory2)):
+                print(f'• {self.inventory2[i].name}')
+            while self.Monstre2 not in self.inventory2:
+                self.Monstre2 = input('Lequel voulez-vous envoyer en premier ? ')
+            for monstre in self.inventory2:
+                if monstre == self.Monstre2:
+                    self.Monstre2 = monstre
+            print(f'Vous avez choisi {self.Monstre2.name} ! {self.Monstre2.name}, go !')
+    
+    
+    # Méthode qui fais un tour de jeu
+    def tour_de_jeu(self):
+        print(f"C'est {self.Monstre1.initiative(self.Monstre2).name} qui commence à jouer !")
+        
+
 
 # tests (à supprimer)
 player1 = player('pandipanda', [MonsterMunch(), MonsterMunch(name='Joueur de LoL'), MonsterMunch(name='Esclave')])
-duel = combat(player1)
+player2 = player('Le Salaud', [MonsterMunch(name='Zigzaton'), MonsterMunch(name='Greou')])
+duel = combat(player1, player2)
 print([MonsterMunch()])
 print(MonsterMunch() == MonsterMunch(name='Generic Monster'))
 print(MonsterMunch() == MonsterMunch(name='bato'))
@@ -32,3 +64,4 @@ print(MonsterMunch() == 'Generic Monster')
 
 # lancement du combat
 duel.debut()
+duel.tour_de_jeu()
